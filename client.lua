fb42a0d401fd8a7aac80937471c62806423dff29
Tronix <62103846+AxelTronix@users.noreply.github.com>
2021-12-08 19:10:35 +0000

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,10 +1,16 @@
--- You can add more Screen resolutions here
+----------------------------------------------
+-- You can add more Screen resolutions here --
+----------------------------------------------
 
 local BlacklistedResolution = {
     [1] = {x = 1280,y = 768},
     [2] = {x = 1280,y = 1024},
     [3] = {x = 1280,y = 960},
 }
+
+----------------------------------------------
+---------  C I T I Z E N T H R E A D  --------
+----------------------------------------------
 
 Citizen.CreateThread(function()
     while true do
